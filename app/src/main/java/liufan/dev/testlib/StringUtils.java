7946975dd820e26d07adcb6f16a23facd2307c71
刘帆 <liufan@liufandeMacBook-Pro.local>
2016-09-05 17:02:10 +0800

    Initial commit

---
+++ app/src/main/java/liufan/dev/testlib/StringUtils.java
@@ -0,0 +1,12 @@
+package liufan.dev.testlib;
+
+import android.text.TextUtils;
+
+/**
+ * Created by liufan on 16/9/5.
+ */
+public class StringUtils {
+    public static boolean isEmpty(String source) {
+        return TextUtils.isEmpty(source);
+    }
+}
